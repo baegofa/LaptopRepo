c8055f55545661ed1521f479b51cdad77cc95ead
baegofa <parkgw2004@gmail.com>
2026-04-02 10:50:35 +0900

    아아

---
+++ src/JavaLecturePractice/overridingPractice.java
@@ -0,0 +1,29 @@
+package JavaLecturePractice;
+
+class SuperObject {
+	protected String name;
+	public void paint() {
+		draw();
+	}
+	
+	public void draw() {
+		System.out.println(name);
+	}
+}
+
+public class overridingPractice extends SuperObject{
+	protected String name;
+	@Override
+	public void draw() {
+		name = "Sub";
+		super.name = "Super";
+		super.draw();
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		SuperObject b = new overridingPractice();
+		b.paint();
+	}
+
+}
--- src/chapter4Quiz/WordGameApp.java
+++ src/chapter4Quiz/WordGameApp.java
@@ -1,9 +1,41 @@
 package chapter4Quiz;
+import java.util.Scanner;
+
+class addPlayer {
+	Scanner scanner = new Scanner(System.in);
+	int playerNum;
+	String name;
+	
+	addPlayer () {
+		playerNum = 0;
+		name = "";
+	}
+	
+	public String[] writePlayerList () {
+		System.out.println("게임 참가 인원을 입력하세요:");
+		playerNum = scanner.nextInt();
+		
+		String[] player = new String[playerNum];
+		
+		for (int i=0;i<playerNum;++i) {
+			System.out.println("참가자의 이름을 입력하세요:");
+			player[i] = scanner.nextLine();
+		}
+		return player;
+	}
+}
+
+class wordCompare {
+	
+}
+
+class decideWin {
+	
+}
 
 public class WordGameApp {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("잠좀 자자");
+		addPlayer a = new addPlayer();
+		a.writePlayerList();
 	}
-
 }
