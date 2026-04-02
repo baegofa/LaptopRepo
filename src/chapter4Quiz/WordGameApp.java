58b0b50b515c07a361c8f8e72711c7b260850c40
baegofa <parkgw2004@gmail.com>
2026-04-02 13:53:35 +0900

    %3은 3명일 때만 동작하므로 playerNum으로 바꿈. 매직넘버의 중요성

--- src/chapter4Quiz/WordGameApp.java
+++ src/chapter4Quiz/WordGameApp.java
@@ -23,7 +23,7 @@ class player {
 
 //단어를 입력받아 저장하는 메소드
 	public void updatePlayerWord(int count,String input) {
-		playerWordList[count%3][1] = input;
+		playerWordList[count%playerNum][1] = input;
 	}
 //단어를 비교하는 메소드
 	public boolean compareWord(int count) {
@@ -31,7 +31,7 @@ class player {
 		if(count<1) return true;
 		
 		else {
-			if(count%3 == 0) {
+			if(count%playerNum == 0) {
 				lastWord = playerWordList[2][wordSection];
 				nowWord = playerWordList[0][wordSection];
 				wordIndex = lastWord.length();
@@ -40,8 +40,8 @@ class player {
 				
 			}
 			else {
-				lastWord = playerWordList[count%3-1][wordSection];
-				nowWord = playerWordList[count%3][wordSection];
+				lastWord = playerWordList[count%playerNum-1][wordSection];
+				nowWord = playerWordList[count%playerNum][wordSection];
 				wordIndex = lastWord.length();
 				lastchar = lastWord.charAt(wordIndex-1);
 				nowchar = nowWord.charAt(0);
@@ -55,14 +55,14 @@ class player {
 	}
 //패자를 가리는 메소드
 	public void decideLoser(int count) {
-		loser = playerWordList[count%3][0];
-		System.out.println("패자는" + loser + "입니다.");
+		loser = playerWordList[count%playerNum][0];
+		System.out.println("패자는 " + loser + "입니다.");
 	}
 //실행하는 메소드
 	public void run() {
 		addPlayer();
 		while(compareWord(count-1)) {
-			System.out.println(playerWordList[count%3][nameSection]+"님 단어를 입력하세요:");
+			System.out.println(playerWordList[count%playerNum][nameSection]+"님 단어를 입력하세요:");
 			inputWord = scanner.next();
 			updatePlayerWord(count,inputWord);
 			++count;
