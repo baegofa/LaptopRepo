bc0e33368e50f701fa87f9f01598c77343ad2eef
baegofa <parkgw2004@gmail.com>
2026-04-03 22:08:19 +0900

    무한 반복 문제 디버깅, 매니저에 숫자를 안쓰는 기능 구현할 예정

--- src/chapter4Quiz/Reservation.java
+++ src/chapter4Quiz/Reservation.java
@@ -16,24 +16,28 @@ public class Reservation {
 	public static final boolean DEBUG = true;
 	Scanner scanner = new Scanner(System.in);
 	Seat[] seat = new Seat[TOTALTABLENUMBER];
+	int reservedNumber = 0;
 	
 	//생성자
 	Reservation() {
-		for (int i=0;i<TOTALTABLENUMBER;++i) {
-			seat[i] = null;
-		}
+		
 	}
 	
 	//예약자 받기
-	public void booking(int tableNum) {
+	public boolean booking(int tableNum) {
 		String name;
 		int peerNum;
 		System.out.print("예약자 이름과 고객수를 입력하세요>>");
 		name = scanner.next();
 		peerNum = scanner.nextInt();
-		if (name.equals("매니저")) callManager();
+		if (name.equals("매니저")) {
+			callManager();
+			return false;
+		}
 		else {
 			makeSeat(tableNum,name,peerNum);
+			++reservedNumber;
+			return true;
 		}
 	}
 	
@@ -46,15 +50,15 @@ public class Reservation {
 		System.out.println("현재 예약자를 출력합니다.");
 		for (int i=0;i<TOTALTABLENUMBER;++i) {
 			if(seat[i] == null) {
-				System.out.println("현재 예약된 좌석을 모두 출력하였습니다.");
 				break;
 			}
 			else {
 				System.out.print("좌석 "+(i+1)+": "+seat[i].name+" "+seat[i].num+"명 ");
 			}
 		}
-		
+		System.out.print("\n");
 	}
+	
 	//예약자 출력
 	public void showResevation() {
 		for (int i=0;i<TOTALTABLENUMBER;++i) {
@@ -64,10 +68,8 @@ public class Reservation {
 	
 	public void runReservationSystem() {
 		System.out.println("명품 레스토랑의 예약 시스템입니다. 좌석수는 5개입니다.");
-		while(seat[4] == null) {
-			int reserveNum = 0;
-			booking(reserveNum);
-			++reserveNum;
+		for (int i=0;i<TOTALTABLENUMBER;++i) {
+			if (booking(i) == false) --i;
 		}
 		System.out.println("만석입니다. 예약을 종료합니다.");
 		showResevation();
