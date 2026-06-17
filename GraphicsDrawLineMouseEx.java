5fd418582792fdcd0950e66f84e6732e9495269d
baegofa <parkgw2004@gmail.com>
2026-06-17 10:29:27 +0900

    마우스로 선그리기

---
+++ GraphicsDrawLineMouseEx.java
@@ -0,0 +1,64 @@
+package test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class GraphicsDrawLineMouseEx extends JFrame{
+	public GraphicsDrawLineMouseEx() {
+		setTitle("마우스로 여러 개의 선 그리기 에제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(new MyPanel());
+		
+		setSize(300,300);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new GraphicsDrawLineMouseEx();
+	}
+	
+	class MyPanel extends JPanel {
+		private Vector<Point> vStart = new Vector<Point>();
+		private Vector<Point> vEnd = new Vector<Point>();
+		Point tempP;
+		
+		public MyPanel() {
+			addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					Point startP = e.getPoint();
+					vStart.add(startP);
+				}
+				
+				public void mouseReleased(MouseEvent e) {
+					Point endP = e.getPoint();
+					vEnd.add(endP);
+					repaint();
+				}
+			});
+			addMouseMotionListener(new MouseAdapter() {
+				public void mouseDragged(MouseEvent e) {
+					vEnd.add(e.getPoint());
+					repaint();
+					vStart.add(e.getPoint());
+				}
+			});
+			
+		}
+		
+		public void paintComponent(Graphics g) {
+			g.setColor(Color.BLUE);
+			super.paintComponent(g);
+			for (int i=0; i<vEnd.size();++i) {
+				Point s = vStart.elementAt(i);
+				Point e = vEnd.elementAt(i);
+				
+				g.drawLine((int)s.getX(),(int)s.getY(),(int)e.getX(),(int)e.getY());
+				
+			}
+		}
+		
+	}
+
+}
